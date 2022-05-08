a98c42003154d4d65e78665cbdb2e883e8af22ec
john cobain <andreygdsn@gmail.com>
2022-05-08 19:11:48 -0300

    aula5

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,38 @@ onScroll();
 function onScroll(){
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section){
+  const targetLine = scrollY + innerHeight /2;
+
+  //verificar se a seção passou da linha
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  // verificar se a base esta abaixo da linha
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+
+  //limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove('active');
+  if(sectionBoundaries){
+    menuElement.classList.add('active');
+  }
+
 }
 
 function showNavOnScroll(){
@@ -27,7 +59,7 @@ function openMenu(){
 }
 
 function closeMenu(){
-  document.body.classList.remove('menu-expanded')
+  document.body.classList.remove('menu-expanded');
 }
 
 
